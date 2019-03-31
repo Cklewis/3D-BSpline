ca1e15a169466322a5954c2ca90b88ec5b363023
Acecoder <vokevin91@gmail.com>
2019-03-31 12:42:37 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import interpolate
+
+#Extra data from excel and put into array
+xPoints = [0, 1, 2, 3, 4, 5]
+yPoints = [12, 14, 22, 39, 58, 77]
+tck = interpolate.splrep(xPoints, yPoints, s=0)
+xnew = [0, 1, 2, 3, 4, 5]
+ynew = interpolate.splev(xnew, tck, der = 0)
+
+plt.figure()
+plt.plot(xPoints, yPoints, 'x', xnew, ynew, xnew, np.sin(xnew), xPoints, yPoints, 'b')
+plt.legend(['Linear', 'Cubic Spline', 'True'])
+plt.axis('auto')
+plt.title('Cubic-Spline Interpolation')
+plt.show()
